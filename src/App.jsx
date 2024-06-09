01d1765408ef34aa34f27299f8555d8d88a3921b
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-06-09 10:56:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
-import Feedback from "./components/Feedback/Feedback";
-
+import Phonebook from "./components/Phonebook/Phonebook";
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
 
-  const handleButtonClick = (type) => {
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      [type]: prevFeedback[type] + 1,
-    }));
+  useEffect(() => {
+    const localContacts = localStorage.getItem("contacts");
+    if (localContacts) {
+      setContacts(JSON.parse(localContacts));
+    } else {
+      setContacts([
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ]);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSubmit = (newContact) => {
+    const isContactInTheList = contacts.some(
+      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (!isContactInTheList) {
+      setContacts((prevState) => [...prevState, newContact]);
+    } else {
+      alert("Contact with the same name already exists!");
+    }
   };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleDelete = (id) => {
+    setContacts((prevState) => prevState.filter((contact) => contact.id !== id));
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <>
-      <Feedback feedback={feedback} onFeedbackChange = {handleButtonClick} />
-    </>
+    <Phonebook
+      contacts={contacts}
+      filteredContacts={filteredContacts}
+      filter={filter}
+      onFilterChange={handleFilterChange}
+      onSubmit={handleSubmit}
+      onDelete={handleDelete}
+    />
   );
 };
 
